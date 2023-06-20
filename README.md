352dea41c7cea5b4daaaf1cd1cbce0dc0670eb6f
Ernesto Reis <79682382+ernestosrf@users.noreply.github.com>
2023-06-20 01:41:03 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+Jogo de advinhação de palavras feito com React.
+Pode ser jogado no link: https://ernestosrf.github.io/series-game/
+
+Atualmente as categorias são personagens de animes e séries. Contém as seguintes:
+- Boku no Hero
+- Shingeki no Kyojin
+- Kimetsu no Yaiba
+- Game of Thrones
+- Supernatural
+- The Middle
---
+++ src/data/words.js
@@ -0,0 +1,13 @@
+export const wordsList = {
+    sala: ["Grupo4e5", "Ciências", "Português", "Matemática", "História", "Geografia", "Secretaria", "Artes"],
+    professores: ["Rejane", "Cristiane", "Leidivania", "Débora", "Priscila", "Patrícia", "Queila", "Josenildes", "Luan", "Luciana", "Maiara", "Viviana"],
+    alunos: ["Elkeson", "João", "Roberto", "Adrielle", "Michel", "Josemar", "Bruna", "Pedro", "Lara", "Maria", "George", "Wyllian", "Marcos", "Rafaela"],
+    desenhos_animados: ["Bob-Esponja", "Os-Simpsons", "Dora-Aventureira", "Turma-da-Mônica"],
+    o_que_sinto_na_escola: ["Sono", "Alegria", "Segurança", "Tristeza", "Diversão", "Cansaço", "Preguiça", "Calma", "Raiva"],
+   o_que_matemática_tem: ["Divisão", "Cálculo", "Soma", "Subtração", "Potência", "Raiz", "Números"],
+    filmes_animados: ["Moana", "Frozen", "Red", "Divertidamente", "Rei-Leão", "Minions"],
+   o_que_tem_em_ciências: ["Experimento", "Leitura", "Célula", "Vírus", "Microscópio", "Animais", "Plantas"],
+   o_que_tem_em_português: ["Leitura", "Escrita", "Redação", "Poema", "Histórias", "Letras"],
+   o_que_geografia_tem: ["Interpretação", "Mapas", "Debate", "Bussola"],
+   na_escola_eu_gosto_de: ["Conversar", "Brincar", "Rir", "Estudar", "Ler", "Escrever", "Jogar"],
+};
